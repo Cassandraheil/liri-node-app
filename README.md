56df4c7f7e3a3810fcc700f8569c15af8655eada
Cassandra Heil <Cassandraheil19@gmail.com>
2020-02-29 10:03:33 -0500

    got the last command working

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # liri-node-app
 
 The Problem-
-    Like all apps,  this app has a purpose; to solve a problem. A big problem with today's society is the overload of data spread sporatically. Conveniently, this app brings together information about movies, songs, artists, and the artists' events.
+    Like all apps, this app has a purpose; to solve a problem. A big problem with today's society is the overload of data spread sporatically across the internet. Conveniently, this app brings together information about movies, songs, artists, and the artists' events into one easy to use package.
 2. Give a high-level overview of how the app is organized
 An Overview-
     This app has many files intertwined together to make it function properly....
@@ -9,11 +9,10 @@ An Overview-
 Instructions-
     1)open the app in your terminal
     2)pick the information you want using these options
-        *concert-this
-        *spotify-this-song
-        *movie-this
-        *do-what-it-says
-            (descriptions provided in the overview section)
+        **'movie-this'--- will use the OMDB api to take in a movie title and return the title, the year released, the IMDB rating, the Rotten Tomatoe rating, the country where the movie was produced, language of the movie, the plot, and the Actors in the movie. 
+        **'spotify-this-song'--- will use the Spotify api take in a song name and output the artist, the song name, a link to preview the song, and the album the song is on. 
+        **'concert this' --- will use the Bands in Town api to take in an artist or band name and output the name of the venue, venue location, and date of the event.
+        **'do what it says' --- will call whatever command is running in the random.txt file
     3)type 'node liri.js ' 
     4)then your pick 'concert-this '
     5)then type your relevant topic 'Ariana Grande'
@@ -26,7 +25,7 @@ Link-
     https://cassandraheil.github.io/liri-node-app/
 6. Clearly list the technologies used in the app
 Technologies-
-    This app utilizes a variety of highlevel resources used by coders daily...
+    This app utilizes a variety of high-level resources used by coders daily...
 7. State your role in the app development
 My Part-
     This app and most modern apps could not happen without collaboration and the openness of the coding community. 
--- liri.js
+++ liri.js
@@ -2,20 +2,25 @@ require("dotenv").config();
 
 // ---grabbing axios 
 var axios = require("axios");
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 
 
 var firstArg = process.argv[2];
-var secArg = process.argv[3];
+var secArg = [];
+for (var i=3; i<process.argv.length; i++){
+    secArg.push(process.argv[i]);
+}
+secArg = secArg.join(" ")
 
 function spotify(){
 //--------------Spotify aPI--------------------------
-var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 spotify.search({ type: 'track', query: secArg })
 .then(function(response) {
-    console.log("Artist name: ", secArg); // response path
+    console.log("Artist", response.tracks.items[0]); // response path
     // console.log("Song Name: ", response);
     // console.log("Preview the song?.... ", response);
     // console.log("The Album: ", response.album.artist);
@@ -26,26 +31,26 @@ spotify.search({ type: 'track', query: secArg })
 };
 
 
-function omdbAPI(){
-//   --------omdbAPI below----------------------------
-axios.get("http://www.omdbapi.com/?t="+ secArg +"&y=&plot=short&apikey=4526150c").then(
-  function(response) {
-    console.log("Title: " + response.data.Title);
-    console.log("Release year: " + response.data.Year); // or release?!?!?
-    console.log("IMDB's rating is: " + response.data.imdbRating);
-    console.log("Rotten Tomato's rating is: " + response.data.Ratings[1].Value);
-    console.log("country produces in: " + response.data.Country);
-    console.log("language: " + response.data.Language);
-    console.log("plot: " + response.data.Plot);
-    console.log("actors: " + response.data.Actors);
-  })  .catch(function(error) {
+function omdbAPI(movie){
+    //   --------omdbAPI below----------------------------
+    axios.get("http://www.omdbapi.com/?t="+ movie +"&y=&plot=short&apikey=4526150c").then(
+        function(response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Release year: " + response.data.Year); // or release?!?!?
+            console.log("IMDB's rating is: " + response.data.imdbRating);
+            console.log("Rotten Tomato's rating is: " + response.data.Ratings[1].Value);
+            console.log("country produces in: " + response.data.Country);
+            console.log("language: " + response.data.Language);
+            console.log("plot: " + response.data.Plot);
+            console.log("actors: " + response.data.Actors);
+})  .catch(function(error) {
     if (error.response) {
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
     } else if (error.request) {console.log(error.request);
     } else {console.log("Error", error.message);
 } console.log(error.config);
@@ -54,24 +59,25 @@ axios.get("http://www.omdbapi.com/?t="+ secArg +"&y=&plot=short&apikey=4526150c"
 
 function bandsinTown(){
     //---------------bands in town API-------------
-axios.get("https://rest.bandsintown.com/artists/" + secArg + "/events?app_id=codingbootcamp").then(
-  function(response) {
-    console.log("The artist's name is" + response.name);
-    // console.log("venue location" + response.url);
-    // console.log("date of event(MM/DD/YYY)" + response);
-  })  .catch(function(error) {
-    if (error.response) {
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {console.log(error.request);
-    } else {console.log("Error", error.message);
-    } console.log(error.config);
-  });// ------Bands In Town End API--------------------
+    axios.get("https://rest.bandsintown.com/artists/" + secArg + "/events?app_id=codingbootcamp").then(
+        function(response) {
+            console.log("The artist's name is" + response.name);
+            // console.log("venue location" + response.url);
+            // console.log("date of event(MM/DD/YYY)" + response);
+        })  .catch(function(error) {
+            if (error.response) {
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {console.log(error.request);
+            } else {console.log("Error", error.message);
+        } console.log(error.config);
+    });// ------Bands In Town End API--------------------
 };
+
 
 //calling the spotify funstion or the default
 if (firstArg === "spotify-this-song" && secArg === ""){
@@ -84,26 +90,51 @@ else if (firstArg === "spotify-this-song"){
 
 //calling omdb function or the default
 else if (firstArg ==="movie-this" && secArg === ""){
-        var secArg = "Mr. Nobody";
-        console.log("this is happening")
-        // omdbAPI();
+    omdbAPI("Mr. Nobody");
 }
 else if (firstArg ==="movie-this"){
-    // omdbAPI();
-    console.log("jk this one is happening")
+    omdbAPI(secArg);
 }
-        
+
 //calling the Bands in Town function, or the default
-else if (firstArg === "concert-this" && secArg === " "){
+else if (firstArg === "concert-this" && secArg === ""){
     secArg === "Ariana Grande"
 }
 else if (firstArg === "concert-this"){
     bandsinTown();
 }
 
-else{
-    console.log("sorry, retype that");
-}
+//calling the random txt file
+else if (firstArg === "do-what-it-says"){
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {return console.log(error);}
+            
+            var dataArr = data.split(",");          // data.split(""""), then remove the index of where they might be          
+            //remove.Item("''", dataArr)                 how to get rid of quotations???
+            firstArg =dataArr[0];
+            secArg =dataArr[1];
+
+
+            if (firstArg === "spotify-this-song"){
+                spotify();
+            }
+            else if (firstArg ==="movie-this" && secArg === ""){
+                omdbAPI("Mr. Nobody");
+            }
+            else if (firstArg ==="movie-this"){
+                omdbAPI(secArg);
+                console.log("jk this one is happening")
+            }
+            else if (firstArg === "concert-this"){
+                bandsinTown();
+            }            
+        });
+    }
+    
+    else{
+        console.log("sorry, retype that. Make sure you spelled everything correctly.");
+    }
 
 
 
